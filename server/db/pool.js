f8d8683dc9c1342704fb52f9a8f290f15b79cd3d
Gabriel Afotey <gabrielafotey@gmail.com>
2025-10-21 14:23:42 +0000

    ssl added

--- server/db/pool.js
+++ server/db/pool.js
@@ -2,7 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL
+    connectionString: process.env.DB_URL,
+     ssl: { rejectUnauthorized: false }
 });
 
 
