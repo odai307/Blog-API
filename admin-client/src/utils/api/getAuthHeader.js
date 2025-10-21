b2bafb59d7d76dde45efc9e4e85a8b847efaad92
Gabriel Afotey <gabrielafotey@gmail.com>
2025-10-21 15:42:14 +0000

    updated api url

--- admin-client/src/utils/api/getAuthHeader.js
+++ admin-client/src/utils/api/getAuthHeader.js
@@ -7,7 +7,10 @@ const getAuthHeader = () => {
   }
 }
 
-const API_BASE_URL = 'http://localhost:3000/api';
+// const API_BASE_URL = 'http://localhost:3000/api';
+// const API_BASE_URL = 'https://blog-api-chms.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 
 
 
